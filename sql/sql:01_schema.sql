70a8a3bee7d6a6e239eda96c2ebf65f6d2359c58
Ferhat Koç <ferhattkoc16@icloud.com>
2026-01-20 16:04:02 +0300

    Update sql:01_schema.sql

--- sql/sql:01_schema.sql
+++ sql/sql:01_schema.sql
@@ -1,10 +1,18 @@
-SELECT d.departman_adi,
-       COUNT(*) AS toplam_sayisi
-FROM calisan_table ct
-JOIN departman d ON ct.dep_id = d.dep_id
-GROUP BY d.departman_adi
-ORDER BY toplam_sayisi DESC;
+-- calisan_db.calisan_table definition
 
-
-
-SELECT COUNT(*) FROM calisan_table;
+CREATE TABLE `calisan_table` (
+  `calisan_id` int NOT NULL,
+  `dep_id` int DEFAULT NULL,
+  `sehir_id` int DEFAULT NULL,
+  `telefon_tur_id` int DEFAULT NULL,
+  `dogumtarihi` date DEFAULT NULL,
+  `maas` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`calisan_id`),
+  KEY `fk_calisan_dep` (`dep_id`),
+  KEY `fk_calisan_sehir` (`sehir_id`),
+  KEY `fk_calisan_tel` (`telefon_tur_id`),
+  CONSTRAINT `fk_calisan` FOREIGN KEY (`calisan_id`) REFERENCES `calisan` (`calisan_id`),
+  CONSTRAINT `fk_calisan_dep` FOREIGN KEY (`dep_id`) REFERENCES `departman` (`dep_id`),
+  CONSTRAINT `fk_calisan_sehir` FOREIGN KEY (`sehir_id`) REFERENCES `sehir` (`sehir_id`),
+  CONSTRAINT `fk_calisan_tel` FOREIGN KEY (`telefon_tur_id`) REFERENCES `telefon_tur_pk` (`telefon_tur_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
